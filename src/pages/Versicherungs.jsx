5da6f9692c854f27b7fcc7456b85ecaa61af7748
Naser <108549635+naser0o0@users.noreply.github.com>
2024-01-29 17:02:19 +0100

    aysinc function

--- src/pages/Versicherungs.jsx
+++ src/pages/Versicherungs.jsx
@@ -9,56 +9,67 @@ import "./Versicherungs.css";
 
 export default function Versicherungs() {
   // const [sliderValue, setSliderValue] = useState(100);
-    const { geburtsdatum, sliderValue, setSliderValue } = useAppContext();
+  const { geburtsdatum, sliderValue, setSliderValue } = useAppContext();
 
-    const navigate = useNavigate();
-     function navigationHandler() {
-       navigate("/");
-     }
+  const navigate = useNavigate();
+  function navigationHandler() {
+    navigate("/");
+  }
 
   // console.log(sliderValue);
 
   const handleSubmit = async () => {
-    const dataToSend = {
-      tarif: {
-        name: "sterbegeld",
-      },
-      versicherungsnehmer: {
+    // Konvertierung des Geburtsdatums aus dem Context in ein Date-Objekt
+    const geburtsdatumParts = geburtsdatum.split(".");
+    const geburtsdatumDate = new Date(
+      geburtsdatumParts[2],
+      geburtsdatumParts[1] - 1,
+      geburtsdatumParts[0]
+    );
+
+    // Berechnung des Alters basierend auf dem Geburtsdatum
+    const heute = new Date();
+    let alter = heute.getFullYear() - geburtsdatumDate.getFullYear();
+    const m = heute.getMonth() - geburtsdatumDate.getMonth();
+    if (m < 0 || (m === 0 && heute.getDate() < geburtsdatumDate.getDate())) {
+      alter--;
+    }
+
+    // Überprüfung, ob das Alter zwischen 50 und 80 Jahren liegt
+    if (alter >= 50 && alter <= 80) {
+      const dataToSend = {
         geburtsdatum: `${geburtsdatum.day}.${geburtsdatum.month}.${geburtsdatum.year}`,
-      },
-      vertragsdaten: {
-        endalter: 85,
-        versicherungsbeginn: "01.10.2023",
         versicherungssumme: sliderValue,
-        zahlungsweise: "monatlich",
-      },
-    };
+      };
 
-    try {
-      const response = await fetch(
-        "https://t3a.hannoversche.de/api/v2/taa/quote",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(dataToSend),
+      try {
+        const response = await fetch(
+          "https://t3a.hannoversche.de/api/v2/taa/quote",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataToSend),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Netzwerkantwort war nicht ok.");
         }
-      );
 
-      if (!response.ok) {
-        throw new Error("Netzwerkantwort war nicht ok.");
+        const responseData = await response.json();
+        console.log(responseData);
+        navigate("/tarif"); // Navigieren Sie zur nächsten Seite nach erfolgreichem API-Aufruf
+      } catch (error) {
+        console.error("Fehler beim Senden der Daten:", error);
       }
-      console.log('response :', response);
-
-      const responseData = await response.json();
-      console.log(responseData);
-      navigate("/tarif"); // Navigieren Sie zur nächsten Seite nach erfolgreichem API-Aufruf
-    } catch (error) {
-      console.error("Fehler beim Senden der Daten:", error);
+    } else {
+      console.error("Das Alter muss zwischen 50 und 80 Jahren liegen.");
     }
   };
-  console.log("sliderValue : ", sliderValue);
+
+
 
   return (
     <section className="section_v">
