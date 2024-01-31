abeb28b5d893aaaca56a1af8ec30a4f2848a548d
Naser <108549635+naser0o0@users.noreply.github.com>
2024-01-31 12:13:47 +0100

    input value focus fixing

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,14 +1,34 @@
 import './InputField.css';
+import { useState } from 'react';
 
 export default function InputField ({ type, name, placeholder, value, onChange,error }) {
+ const [isFocused, setIsFocused] = useState(false);
+
+ const handleFocus = () => {
+   setIsFocused(true);
+ };
+
+ const handleBlur = () => {
+   if (!value) {
+     setIsFocused(false);
+   }
+ };
   return (
     <input
       type={type}
       name={name}
       className={`input ${error ? "input-error" : ""}`}
+      style={{
+        border: isFocused
+          ? "2px solid #yourFocusedColor"
+          : "1px solid #yourNormalColor",
+        backgroundColor: isFocused ? "#DAE1EC" : "",
+      }}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
     />
   );
 }
